fea9031bd2969a4823360f91c0409e771f806bb9
mellow65 <126365319+mellow65@users.noreply.github.com>
2024-08-17 19:42:01 -0700

    Update deb_email.sh

--- deb_email.sh
+++ deb_email.sh
@@ -6,16 +6,16 @@ reset='\033[0m'
 echo -e "${color}By using this script, you'll update the system, download python3, mailutils, pip, discord-webhook.  This will pull down the python script and place it in a folder /home/scripts.  Set up your crontab to run the check three times a day.${reset}"
 
 
-apt update
-apt upgrade -y
+apt-get update
+apt-get upgrade -y
 
-apt install mailutils -y
+apt-get install mailutils -y
 
-apt install python3 -y
+apt-get install python3 -y
 
-apt install pip -y
+apt-get install pip -y
 
-pip install discord-webhook -y
+pip install discord-webhook
 
 wget https://raw.githubusercontent.com/mellow65/deb_mail_webhook/main/discord_newemail.py
 
